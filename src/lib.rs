ce5c014fd48b49180090a67f3934fb012e29d71e
Nik Revenco <pm@nikrev.com>
2025-10-05 06:11:52 +0100

    docs: add comparison section + IDE support

--- src/lib.rs
+++ src/lib.rs
@@ -67,6 +67,11 @@
 //! }
 //! ```
 //!
+//! # IDE Support
+//!
+//! - `rustfmt` formats it flawlessly
+//! - `rust-analyzer` supports hover and goto definition on the actual strings
+//!
 //! # Auto-generated doc comments
 //!
 //! Use `#[display(doc)]` to automatically generate `///` comments. The above example's expansion `enum` would generate this:
@@ -123,6 +128,75 @@
 //!             )),
 //!         }
 //!     }
+//! }
+//! ```
+//!
+//! # Comparison between `displaystr`, `thiserror` and `displaydoc`
+//!
+//! These 4 errors are **identical**.
+//!
+//! ## `displaystr`
+//!
+//! ```
+//! # use std::io;
+//! use thiserror::Error;
+//! use displaystr::display;
+//!
+//! #[derive(Error, Debug)]
+//! #[display]
+//! pub enum DataStoreError {
+//!     Disconnect(#[from] io::Error) = "data store disconnected",
+//!     Redaction(String) = "the data for key `{_0}` is not available",
+//!     InvalidHeader {
+//!         expected: String,
+//!         found: String,
+//!     } = "invalid header (expected {expected:?}, found {found:?})",
+//!     Unknown = "unknown data store error",
+//! }
+//! ```
+//!
+//! ## `thiserror`
+//!
+//! ```
+//! # use std::io;
+//! use thiserror::Error;
+//!
+//! #[derive(Error, Debug)]
+//! pub enum DataStoreError {
+//!     #[error("data store disconnected")]
+//!     Disconnect(#[from] io::Error),
+//!     #[error("the data for key `{0}` is not available")]
+//!     Redaction(String),
+//!     #[error("invalid header (expected {expected:?}, found {found:?})")]
+//!     InvalidHeader {
+//!         expected: String,
+//!         found: String,
+//!     },
+//!     #[error("unknown data store error")]
+//!     Unknown,
+//! }
+//! ```
+//!
+//! ## `displaydoc`
+//!
+//! ```
+//! # use std::io;
+//! use thiserror::Error;
+//! use displaydoc::Display;
+//!
+//! #[derive(Display, Error, Debug)]
+//! pub enum DataStoreError {
+//!     /// data store disconnected
+//!     Disconnect(#[source] io::Error),
+//!     /// the data for key `{0}` is not available
+//!     Redaction(String),
+//!     /// invalid header (expected {expected:?}, found {found:?})
+//!     InvalidHeader {
+//!         expected: String,
+//!         found: String,
+//!     },
+//!     /// unknown data store error
+//!     Unknown,
 //! }
 //! ```
 
